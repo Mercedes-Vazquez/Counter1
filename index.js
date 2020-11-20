92c2d1bce0619655fa84302ea6ecce1136854ad6
Mercedes <mvazquezsan@gmail.com>
2020-11-20 23:36:01 +0100

    testeo funciones

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var contador=0;
+// var contador=0;
+
+let a = 0;
 function increment()
 {
+    console.log(a++);
     meter++;
     alert('The value of the meter is now: '+ meter);
 }
 
-function decrement()
-{
-    meter--;
-    alert('The value of the meter is now: '- meter);
+// function decrement()
+// {
+//     meter--;
+//     alert('The value of the meter is now: '- meter);
 
-}
+// }

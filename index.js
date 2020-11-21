b44b7465bfbf296a5131de1e38a386b97bbe21f0
Mercedes <mvazquezsan@gmail.com>
2020-11-21 23:24:03 +0100

    style change to h1 h2 and buttons

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// var contador=0;
-
-// let 1 = 0;
-// function increment()
-// {
-//     console.log(1++);
-//     meter++;
-   
-
-// function decrement()
-// {
-//     meter--;
-//     alert('The value of the meter is now: '- meter);
-
-// }
+i= 0
+    
+function displayResultAdd() {
+    
+    document.getElementById("myNumber").innerHTML = ++i;
+    }
+    
+function displayResultDiscount() {
+    document.getElementById("myNumber").innerHTML = --i;
+    }

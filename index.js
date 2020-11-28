0bd0d20e8d59aa1ed36116847e388a6e0fe747e4
Mercedes <mvazquezsan@gmail.com>
2020-11-28 20:57:30 +0100

    place both buttons on same line in HTML

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let i= 0
     
 function displayResultAdd() {
-    
     document.getElementById("myNumber").innerHTML = ++i;
     }
     
 function displayResultDiscount() {
     document.getElementById("myNumber").innerHTML = --i;
     }
+// function showUseOfButtonAdd() {
+
+    
+// }
